64e27226f4272f1bdd6939fced83be30e83b3618
henryyang <16546014+henryhuayang@user.noreply.gitee.com>
2026-02-16 20:26:29 +0800

    change domain

--- EC2-WireGuardVPN/EC2-WireGuardVPN/EC2-WireGuardVPN-ON.cmd
+++
@@ -1,8 +0,0 @@
-﻿@echo off
-:: ==========================================
-:: Launch EC2 on AWS Cloud
-:: ==========================================
-
-set "SCRIPT_DIR=%~dp0"
-
-powershell -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File \"%SCRIPT_DIR%\setup\ec2-new.ps1\"' -Verb RunAs"
--- EC2-WireGuardVPN/EC2-WireGuardVPN/setup/WireGuard_setup.cmd
+++
@@ -1,7 +0,0 @@
-﻿@echo off
-:: ==========================================
-:: Launcher for WireGuard Client VPN Installer
-:: ==========================================
-
-echo Starting WireGuard Installer...
-powershell.exe -ExecutionPolicy Bypass -NoProfile -File "%~dp0WireGuard.ps1"
